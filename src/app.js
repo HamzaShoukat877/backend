3edf7ca79324a5e37873cf7ad9fa118d326800d8
HamzaShoukat877 <hamzashoukat129@gmail.com>
2024-08-20 19:49:08 +0500

    add http

--- src/app.js
+++ src/app.js
@@ -17,4 +17,13 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
-export {app}
+// routes import
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users",userRouter)
+
+// https:localhost:8000/api/v1/users/register
+
+
+export { app }
